52e78519b455e8a41a1ff09be7fa81c0ab190e58
aslin-r <aslinjoelr123@gmail.com>
2023-08-23 22:31:06 +0530

    updated

--- digitSum.c
+++ digitSum.c
@@ -1,30 +1,21 @@
-#include<stdio.h>
-void main(){
-    
-    int num=29999,sum=0,tem;
-   while(num>0){
-        tem=num%10; // 29999 %10=9
-        sum+=tem;// 0+9
-        num/=10;// 2999
-   }
-   num=sum;
-   sum=0;
-   
-   tem=0;
-   while(num>0){
-         tem=num%10;
-         sum+=tem;
-         num/=10;
-   }
-     num=sum;
-   sum=0;
-   
-   tem=0;
-   while(num>0){
-         tem=num%10; 
-         sum+=tem;
-         num/=10;
-   }
-   printf("Sum is : %d",sum);
-   printf("\n Num is : %d",num);
+#include <stdio.h>
+
+int main() {
+    int num = 29999, sum = 0, temp;
+
+    while (num > 9) { // Keep looping until the sum becomes a single-digit number
+        sum = 0; // Reset sum for each iteration
+        temp = num;
+
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+
+        num = sum; // Assign the sum to num for the next iteration
+    }
+
+    printf("Final single-digit sum is: %d", num);
+
+    return 0;
 }
