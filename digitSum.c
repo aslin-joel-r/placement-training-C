f597dbe4e702dcda061180b1611bf9f514cc0ea3
aslin-r <aslinjoelr123@gmail.com>
2023-08-23 22:29:24 +0530

    my code

--- digitSum.c
+++ digitSum.c
@@ -3,9 +3,28 @@ void main(){
     
     int num=29999,sum=0,tem;
    while(num>0){
-        tem=num%10;
-        sum+=tem;
-        num/=10;
+        tem=num%10; // 29999 %10=9
+        sum+=tem;// 0+9
+        num/=10;// 2999
+   }
+   num=sum;
+   sum=0;
+   
+   tem=0;
+   while(num>0){
+         tem=num%10;
+         sum+=tem;
+         num/=10;
+   }
+     num=sum;
+   sum=0;
+   
+   tem=0;
+   while(num>0){
+         tem=num%10; 
+         sum+=tem;
+         num/=10;
    }
    printf("Sum is : %d",sum);
+   printf("\n Num is : %d",num);
 }
