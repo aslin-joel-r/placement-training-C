887e8bad36d08fa202dac79d99eeec42970607cb
aslin-r <aslinjoelr123@gmail.com>
2023-08-24 22:15:05 +0530

    second task algorithm and code works successful

--- primeNumberFinder.c
+++ primeNumberFinder.c
@@ -1,21 +1,19 @@
 #include<stdio.h>
 
 void main(){
-    for(int i=1;i<=14;i++){
-        int temp=0;
+
+    for(int i=1;i<=100;i++){
+      int  temp=0;
         for(int j=1;j<=i;j++){
             if(i%j==0){
-                continue;
+                temp+=1;
             }
             else{
-                temp+=1;
-                 //printf("\nprime number is %d",i);
-            }
-           // printf("\ni is %d and j is %d",i,j);
-            
+                
+            }     
     }
-    printf("\ntemp is %d",temp);
-    if(temp<2){
+    //printf("\ntemp is %d %d",i,temp);
+    if(temp<=2){
         printf("\nprime number is %d",i);
     }
     }
