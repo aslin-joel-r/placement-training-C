c868ddb64ca69a4f29f7865c464822aac85efb44
aslin-r <aslinjoelr123@gmail.com>
2023-08-24 22:20:16 +0530

    solved the issue in the code

--- primeNumberFinder.c
+++ primeNumberFinder.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 
 void main(){
-
-    for(int i=1;i<=100;i++){
-      int  temp=0;
-        for(int j=1;j<=i;j++){
+    printf("The prime numbers are : ");
+    for(int i=2;i<=100;i++){
+      int  factors=0;
+        for(int j=1;j<=i;j++)
+        {
             if(i%j==0){
-                temp+=1;
+                factors+=1; // for checking the no of factors
             }
-            else{
-                
-            }     
-    }
-    //printf("\ntemp is %d %d",i,temp);
-    if(temp<=2){
-        printf("\nprime number is %d",i);
+               
+        }
+    //printf("\ntemp is %d %d",i,temp); // displays the factors
+    if(factors<=2)
+    {
+        printf("\n%d",i);
     }
     }
 }
