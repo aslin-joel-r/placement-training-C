9e033899e4d97f5f8779fad9a504dfc94e286e45
Aslin R <aslinjoelr123@gmail.com>
2023-08-25 09:48:28 +0000

    increment operators

---
+++ incrementOperators.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main(){
+    int a=2;
+    int b=3;
+    printf("\nPost increment %d",a++);
+    printf("\nPost increment %d",a);
+    printf("\nPre increment %d",++b);
+    printf("\nPre increment %d",b);
+}
