a3d21315abf4606b588d7d20332dc1a7771452b5
Aslin R <aslinjoelr123@gmail.com>
2023-08-30 10:34:09 +0000

    completed the task to add the first and the last digit of a number

---
+++ firstLast.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+
+    int num,sum=0,tem;
+    int first,last;
+    
+    printf("Enter a number :");
+    scanf("%d",&num);
+    first = num;
+    while(first>10){
+        first/=10;
+    }
+
+    last=num%10;
+    sum=first+last;
+   
+   printf("\nSum is : %d",sum);
+}
