71e58ba02b369f399e7f80ce87d384a0959d5cc3
Aslin R <aslinjoelr123@gmail.com>
2023-08-30 05:25:54 +0000

    finished the grade calculator

---
+++ grade.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+void main(){
+   int mark;
+   printf("Enter Your Mark :");
+   scanf("%d",&mark);
+   char grade;
+
+
+   if(mark>=90  &&  mark<=100){
+        grade='S';
+        printf("Your Grade is %c",grade);
+   }
+     else if(mark>=80 && mark<90){
+        grade='A';
+        printf("Your Grade is %c",grade);
+   }
+  else if(mark>=70 && mark<80){
+        grade='B';
+        printf("Your Grade is %c",grade);
+   }
+     else if(mark>=60 && mark<70){
+        grade='C';
+        printf("Your Grade is %c",grade);
+   }
+     else if(mark>=50 && mark<60){
+        grade='D';
+        printf("Your Grade is %c",grade);
+   }
+     else{
+        grade='E';
+        printf("Your Grade is %c",grade);
+   }
+}
--- incrementOperators.c
+++ incrementOperators.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 void main(){
-    int a=10,b=12,c=13;
-    int d=(a++ || ++b || ++c);
+    int a=0,b=1,c=2;
+
     // printf("\nPost increment %d",a++);
     // printf("\nPost increment %d",a);
     // printf("\nPre increment %d",++b);
-    printf("\nPre increment %d",d);
+
     printf("\nc  %d",c);
     printf("\nb %d",b);
     printf("\na %d",a);
