de311e5df31de70d22dbb29a58d6440974d1f6ec
Aslin R <aslinjoelr123@gmail.com>
2023-08-31 08:44:32 +0000

    updated the code

--- diffOddEven.c
+++ diffOddEven.c
@@ -5,22 +5,22 @@ void main(){
     int num;
 
     int even=0,odd=0,temp,rem;
-     printf("Enter a number :");
+    printf("Enter a number :");
     scanf("%d",&num);
-    while(num>0){
+    while(num>0)
+    {
         rem=num%10;
         num/=10;
-    if(rem%2==0){
-        even+=rem;
+            if(rem%2==0)
+            {
+                even+=rem;
+                }
+            else{
+                odd+=rem;
+                }
     }
-    else{
-        odd+=rem;
-    }
-    }
-   // printf("Odd sum is %d",odd);
-
-   // printf("\nEven sum is %d",even);
+   
     int diff=even-odd;
     diff=abs(diff);
- printf("\nDifference is : %d",diff);
+    printf("\nDifference is : %d",diff);
 }
