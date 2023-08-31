a692b82431ff64c23a05caf2fea2f8b7430f7c4c
aslin-r <aslinjoelr123@gmail.com>
2023-08-31 21:08:24 +0530

    updated the pallindrome problem

--- pallindrome.c
+++ pallindrome.c
@@ -5,12 +5,18 @@ void main(){
     printf("Enter a number :");
     scanf("%d",&num);
 
-    int temp=num,rem,reverse;
+    int temp=num,rem=0,reverse=0;
 
     while(num>0){
         rem=num%10;
         reverse=reverse*10+rem;
         num/=10;
     }
-    printf("%d Reverse : %d",reverse);
+    printf("Reverse : %d",reverse);
+    if(reverse==temp){
+        printf("\nThe number %d is pallindrome",temp);
+    }
+    else{
+         printf("\nThe number %d is not a pallindrome",temp);
+    }
 }
