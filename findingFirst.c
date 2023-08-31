8fcb7c3dadd66d84d5cdd5ee21dc41426d59b662
Aslin R <aslinjoelr123@gmail.com>
2023-08-31 04:31:23 +0000

    completed another task

---
+++ findingFirst.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+void main(){
+    int num;
+    printf("Enter a number :");
+    scanf("%d",&num);
+    int digit=1;
+    int mod=1;
+    int first=0;
+    while(num>9){
+        num/=10;
+        digit++;
+        mod*=10;
+    }
+     first=num;
+   
+    printf("Number of mod is %d",mod);
+    printf("\nFirst Number  is %d",first);
+}
