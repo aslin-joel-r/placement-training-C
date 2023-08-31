b3472bda77b38dfb8978881c12e8c1e4f10a54be
aslin-r <aslinjoelr123@gmail.com>
2023-08-31 21:15:16 +0530

    updated the noeon problem

--- neonNumber.c
+++ neonNumber.c
@@ -2,18 +2,20 @@
 
 void main(){
     int num;// 9 , 9*9 =81->8+1=>9
-    int temp=num;
+  
     printf("Enter a number :");
+    scanf("%d",&num);
 
+    int temp=num;
     int power=num*num;
-    int rem,sum;
-    while(num>0){
-        rem=num%10;
-        sum+=rem;
-        num/=10;
-    }
-    printf("sum is %d",sum);
+    int rem=0,sum=0;
 
+    while(power>0){
+        rem=power%10;
+        sum+=rem;
+        power/=10;
+    }
+   
     if (sum==temp){
         printf("It is a Neon number");
     }
