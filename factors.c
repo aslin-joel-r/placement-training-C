89976239a2e10a31682900da9bd08bde3489e7a2
Aslin R <aslinjoelr123@gmail.com>
2023-08-31 06:49:25 +0000

    updated the factor program as dynamic

--- factors.c
+++ factors.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 
 void main(){
-    int a=10;
-     printf("Factors are : ");
+    int a;
+    printf("Enter a number :");
+    scanf("%d",&a);
+    printf("Factors are : ");
     for(int i=1;i<=a;i++){
         if(a%i==0){
            printf("\n%d ",i);
