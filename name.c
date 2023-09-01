7cb436498811d82721e0675b5fa87e8e26b0a924
Aslin R <aslinjoelr123@gmail.com>
2023-09-01 06:20:07 +0000

    completed the name task

---
+++ name.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+void main(){
+
+     int num;
+    printf("Enter a number :");
+    scanf("%d",&num);
+   int mid=num%2==0 ? num/2:(num/2)+1;
+  // printf("%d",mid);
+    for(int i=1;i<=num;i++){
+        for(int j=1;j<=num;j++){
+            if(i==1 || j==1 || j==num ||i==mid){
+                    printf("*");
+            }
+            else{
+                printf(" ");
+            }
+    }
+printf("\n");
+}
+
+}
