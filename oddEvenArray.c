b33733a4d30bb953b1e2980c6311856eab02e901
Aslin R <aslinjoelr123@gmail.com>
2023-09-01 06:58:00 +0000

    tried something

--- oddEvenArray.c
+++ oddEvenArray.c
@@ -2,24 +2,27 @@
 
 void main(){
     int size,odd=0,even=0;
+    int ocount=0,ecount=0;
     printf("Enter array size :");
     scanf("%d",&size);
     int arr[size];
     for(int i=0;i<size;i++){
         scanf("%d",&arr[i]);
     }
+     for(int j=0;j<size;j+=1){
+        printf("\n%d",j);
+       
+        // below
+        //even+=arr[j];
+        odd+=arr[j];
+    }
      for(int j=0;j<size;j++){
         printf("\n%d",j);
        
-        if(j%2==0){
-            //printf("- Even number");
-            even+=arr[j];
-        }
-        
-        else{
-            //printf("- Odd number");
-            odd+=arr[j];
-        }
+        // below
+        even+=arr[j];
+        //odd+=arr[j];
+        j+=1;
     }
     printf("\nOdd Sum is : %d",odd);
 
