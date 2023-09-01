ae85aa4ddfda6984f13b592f7659f36e6369bd4b
Aslin R <aslinjoelr123@gmail.com>
2023-09-01 05:12:28 +0000

    solved trhe issue in the x pattern problem

--- xPattern.c
+++ xPattern.c
@@ -2,17 +2,13 @@
 
 void main(){
 
-    int num=8;
+    int num=5;
   
-    for(int i=1;i<num;i++){
-        for(int j=1;j<num;j++){
-            if(i==j){
+    for(int i=1;i<=num;i++){
+        for(int j=1;j<=num;j++){
+            if(i==j || i+j==num+1){
                  
                  printf("*");
-            }
-            else if(i+j==num){
-                
-                printf("*");
             }
            else{
                 printf(" ");
