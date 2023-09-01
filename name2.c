71859ce3d8071569d4d98907b648eb325a81e91b
Aslin R <aslinjoelr123@gmail.com>
2023-09-01 08:47:20 +0000

    updated

--- name2.c
+++ name2.c
@@ -9,11 +9,8 @@ void main(){
 
     for(int i=1;i<=num;i++){
         for(int j=1;j<=num;j++){
-            if(i==1 || j==1  ||i==mid){
+            if(i==1 || j==1 || (j==num && j<=mid) ||i==mid){
                     printf("*");
-            }
-            if(j==num && j<=mid){
-                 printf("*");
             }
             else{
                 printf(" ");
