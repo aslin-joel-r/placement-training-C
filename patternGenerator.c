8fe1d8064fc82011ae49ebd2a077c4d2a7c2d33c
aslin-r <aslinjoelr123@gmail.com>
2023-09-02 11:10:36 +0530

    trying the pattern generator for the alphabets

---
+++ patternGenerator.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+void main(){
+
+    int num;
+    printf("Enter a number :");
+    scanf("%d",&num);
+    int mid=num%2==0 ? num/2:(num/2)+1;
+
+    for(int i=1;i<=num;i++){
+        for(int j=1;j<=num;j++){
+            if(i==1 || j==1 || (j==num && i<=mid)||i==mid || (i==j && i>=mid)){
+                    printf("*");
+            }
+            else{
+                printf(" ");
+            }
+    }
+printf("\n");
+}
+
+}
